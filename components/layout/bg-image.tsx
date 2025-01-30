dfadf762f232d42c0b7dbbeb4c011aeb0c76f457
ayazhankadessova <kadessovaayazhan@gmail.com>
2025-01-30 18:21:35 +0500

    fixed: static import for bg-image

--- components/layout/bg-image.tsx
+++ components/layout/bg-image.tsx
@@ -1,8 +1,8 @@
-"use client"
-
+'use client'
 import Image from 'next/image'
 import { useTheme } from 'next-themes'
-
+import lightBg from '@/public/backgrounds/light-bg-1.jpg'
+import darkBg from '@/public/backgrounds/dark-bg-2.jpg'
 
 export function BackgroundImage() {
   const { theme } = useTheme()
@@ -10,17 +10,13 @@ export function BackgroundImage() {
   return (
     <div className='fixed inset-0 -z-10'>
       <Image
-        src={
-          theme === 'dark'
-            ? '/backgrounds/dark-bg-2.jpg'
-            : '/backgrounds/light-bg-1.jpg'
-        }
+        src={theme === 'dark' ? darkBg : lightBg}
         alt='background'
         quality={100}
         fill
         placeholder='blur'
         sizes='100vw'
-        className='object-cover opacity-40 transition-opacity duration-300'
+        className='object-cover opacity-60 transition-opacity duration-300'
         priority
       />
     </div>
